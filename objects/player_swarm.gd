74eb37b48e74364e4aa3aab7c1b7eb0c44aa3f9b
Maxence Mathieu <Maxence.Mathieu.Mail@allegrodvt.com>
2024-08-17 11:58:49 +0200

    add multiple unit type to swarm

--- objects/player_swarm.gd
+++ objects/player_swarm.gd
@@ -1,16 +1,26 @@
 extends Node
 
-var unitNode = preload("res://objects/unit.tscn");
+var melee_unit = preload("res://objects/unit/melee_unit.tscn");
+var ranged_unit = preload("res://objects/unit/ranged_unit.tscn");
+var shield_unit = preload("res://objects/unit/shield_unit.tscn");
 var units = [];
-var units_number = 30;
+var nb_of_units = 0;
 
 var unit_speed = 0.01;
 var center_of_swarm = Vector2(200, 200);
 
-func initialize_positions(screen_range):
+func initialize_positions(screen_range, nb_of_melee, nb_of_ranged, nb_of_shield):
 	randomize();
-	for i in range(0, units_number):
-		units.append(unitNode.instantiate());
+	nb_of_units = nb_of_melee + nb_of_ranged + nb_of_shield;
+	print(nb_of_units);
+	for i in range(0, nb_of_units):
+		if(i < nb_of_melee):
+			units.append(melee_unit.instantiate());
+		elif(i < nb_of_melee + nb_of_ranged):
+			units.append(ranged_unit.instantiate());
+		elif(i < nb_of_melee + nb_of_ranged + nb_of_shield):
+			units.append(shield_unit.instantiate());
+			
 		units[i].set_global_position(Vector2(randi_range(0, screen_range.x), randi_range(0, screen_range.y)));
 		add_child(units[i]);
 		
@@ -22,11 +32,10 @@ func rule(i, center):
 	
 	
 func _ready(): 
-	initialize_positions(Vector2(500, 500));
 	set_process(true);
 	
 	
 func _process(delta):
-	for i in range(0, units_number):
+	for i in range(0, nb_of_units):
 		var velocity = rule(i, center_of_swarm);
 		units[i].set_linear_velocity(velocity);
