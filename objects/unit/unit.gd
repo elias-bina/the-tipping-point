da9a7e6eaa62d83eb01979eba42696306b730255
elias-bina <elias.bina58@gmail.com>
2024-08-17 21:25:12 +0200

    Added basic sword slash

--- objects/unit/unit.gd
+++ objects/unit/unit.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+class_name Unit
+
 func is_melee():
 	return false;
 	
@@ -8,3 +10,13 @@ func is_ranged():
 	
 func is_shield():
 	return false;
+
+
+
+
+#var angular_force = 50
+#var target = position + Vector2.RIGHT
+#
+#func _physics_process(delta):
+	#var dir = transform.y.dot(position.direction_to(target))
+	#angular_velocity = - dir * angular_force
