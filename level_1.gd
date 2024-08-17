161df8ded0c2a5964580bf9500c78bf02bf970cc
Maxence Mathieu <Maxence.Mathieu.Mail@allegrodvt.com>
2024-08-17 11:58:49 +0200

    add multiple unit type to swarm

--- level_1.gd
+++ level_1.gd
@@ -6,5 +6,5 @@ var swarmNode = preload("res://objects/player_swarm.tscn");
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var swarm = swarmNode.instantiate();
-	swarm.initialize_positions(get_viewport_rect().size);
+	swarm.initialize_positions(get_viewport_rect().size, 10, 20, 10);
 	add_child(swarm);
