b77d21557ebe5b120e4d4e3045c972d3485233db
Maxence Mathieu <Maxence.Mathieu.Mail@allegrodvt.com>
2024-08-17 16:31:22 +0200

    swarm layered

--- objects/unit/ranged_unit.gd
+++ objects/unit/ranged_unit.gd
@@ -1,6 +1,21 @@
-extends "res://objects/player_swarm.gd"
+extends "res://objects/unit/unit.gd"
 
-func rule(i, center):
-	var unit = units[i];
-	var direction_to_center = center - unit.get_global_position();
-	return direction_to_center;
+func get_force_of_repulsion(i, units, center):
+	var velocity_outwards_center = units[i].get_global_position() - center;
+	var amount_of_wrong_neighbors = 0;
+	
+	for j in range(0, units.size()): 
+		if i == j:
+			continue;
+		
+		var distance_between_boids = units[i].get_global_position() - units[j].get_global_position();
+		if distance_between_boids.length() < 30:
+			amount_of_wrong_neighbors += 1;
+		
+	if(amount_of_wrong_neighbors >= 4):
+		return velocity_outwards_center * 50 * -1;
+		
+	return Vector2(0, 0);
+
+func is_ranged():
+	return true;
---
+++ objects/unit/unit.gd
@@ -0,0 +1,10 @@
+extends RigidBody2D
+
+func is_melee():
+	return false;
+	
+func is_ranged():
+	return false;
+	
+func is_shield():
+	return false;
