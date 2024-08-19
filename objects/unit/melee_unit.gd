2280e2e9d2c54d3bdb4dbb79010744d75e8bd3b0
elias-bina <elias.bina58@gmail.com>
2024-08-19 22:29:49 +0200

    Rewrite swarm centering algorithm (This is NOT a boid dammit)
    Test with 3rd unit type

--- objects/unit/melee_unit.gd
+++ objects/unit/melee_unit.gd
@@ -4,40 +4,45 @@ class_name MeleeUnit
 
 var sword_slash = preload("res://objects/projectiles/sword_slash.tscn");
 
+var units_neighbors = []
+
+const repulstion_rate: int = 80
+const reposition_rate: int = 50
+
 func _ready() -> void:
 	$AnimationPlayer.play("down")
 
-func get_force_of_repulsion(i, units, center):
-	var velocity_outwards_center = units[i].get_global_position() - center;
-	var amount_of_wrong_neighbors = 0;
+func get_velocity_modifier(center):
+	var dir_to_center: Vector2 = center - position
+	var nb_wrong_neighbor: int = 0
+	var nb_meh_neighbor: int = 0
+	for unit in units_neighbors:
+		if unit is RangedUnit:
+			if dir_to_center.dot(unit.position - position) < 0:
+				nb_wrong_neighbor += 1
+		if unit is MeleeUnit:
+			if dir_to_center.dot(unit.position - position) < 0:
+				nb_meh_neighbor = 1
 	
-	for j in range(0, units.size()): 
-		if i == j:
-			continue;
-			
-		if units[j].is_melee():
-			continue;
-		
-		var distance_between_boids = units[i].get_global_position() - units[j].get_global_position();
-		if distance_between_boids.length() < 30:
-			
-			if units[j].is_shield():
-				amount_of_wrong_neighbors += 0.8;
-			else: amount_of_wrong_neighbors += 1;
-		
-	if(amount_of_wrong_neighbors >= 4):
-		return velocity_outwards_center * amount_of_wrong_neighbors * 50;
-		
-	return Vector2(0, 0);
+	
+	return -dir_to_center.normalized() * nb_wrong_neighbor * repulstion_rate + \
+		   -dir_to_center.normalized().rotated(PI / 3) * nb_meh_neighbor * reposition_rate;
 
 func attack():
 	var slash = sword_slash.instantiate()
 	slash.set_global_position(Vector2(40,0))
 	add_child(slash)
 
-func is_melee():
-	return true;
+
+func _on_entity_detection_range_body_entered(body: Node2D) -> void:
+	if body is Enemy:
+		look_at(body.global_position)
+	
+	if body is Unit and body != self:
+		units_neighbors.append(body)
 
 
-func _on_ennemy_detection_range_body_entered(body: Node2D) -> void:
-	look_at(body.global_position)
+
+func _on_entity_detection_range_body_exited(body: Node2D) -> void:
+	if body is Unit and body != self:
+		units_neighbors.erase(body)
